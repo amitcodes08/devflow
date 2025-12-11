d06832814267f225b04533b82448512d69d8b364
Amit Gupta <aryaamit387@gmail.com>
2025-12-11 22:50:54 +0530

    implement tag input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -2,28 +2,56 @@ import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import { Badge } from '../ui/badge';
 import { getDeviconClassName } from '@/lib/utils';
+import Image from 'next/image';
 
 interface Props {
     _id : string;
     name: string;
     questions?: number;
     showCount? : boolean;
-    compact? : boolean
+    compact? : boolean;
+    remove? : boolean;
+    isButton? :boolean;
+    handleRemove? : () => void;
 }
 
-const TagCard = ({_id, name, questions, showCount, compact} : Props) => {
+const TagCard = ({_id, name, questions, showCount, compact, remove, isButton, handleRemove} : Props) => {
+
+  const handleClick= (e : React.MouseEvent) => {
+    e.preventDefault();
+  }
 
     const iconClass = getDeviconClassName(name);
+    const Content = (
+      <>
+        <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2">
+          <div className="flex-center spacce-x-2">
+            <i className={`${iconClass} text-sm p-2`}></i>
+            <span>{name}</span>
+          </div>
+          {remove && (
+            <Image src="/icons/close.svg" width={12} height={12} alt="close icon" className='cursor-pointer object-contain invert-0 dark:invert' onClick={handleRemove}/>
+          )}
+        </Badge>
+        {showCount && (
+          <p className="small-medium text-dark500_light700">{questions}</p>
+        )}
+      </>
+    )
+
+    if(compact) {
+        return isButton ? (
+            <button onClick={handleClick} className='flex-between gap-2'>{Content}</button>
+        ) : (
+            <Link href={ROUTES.TAG(_id)} className='flex justify-between gap-2'>
+                {Content}
+            </Link>
+        )
+    }
 
   return (
    <Link href={ROUTES.TAG(_id)} className='flex justify-between gap-2 '>
-    <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
-        <div className='flex-center spacce-x-2'>
-            <i className={`${iconClass} text-sm p-2`}></i>
-            <span>{name}</span>
-        </div>
-    </Badge>
-    {showCount && <p className='small-medium text-dark500_light700'>{questions}</p>}
+    {Content}
     </Link>
   )
 }
--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -51,7 +51,7 @@ const Editor = ({ value, editorRef, fieldChange }: Props) => {
       markdown={value}
       ref={editorRef}
       onChange={fieldChange}
-      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border grid"
       plugins={[
         headingsPlugin(),
         listsPlugin(),
