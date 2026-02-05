c79af813660062cc0a2996b930ea53d729416e37
Amit Gupta <aryaamit387@gmail.com>
2026-02-05 22:18:25 +0530

    Fetch Real Questions Details

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,35 +1,52 @@
-import ROUTES from "@/constants/routes"
-import Link from "next/link"
-import { Avatar, AvatarFallback } from "./ui/avatar"
-import Image from "next/image"
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+import ROUTES from '@/constants/routes'
+import { cn } from '@/lib/utils'
+
+import { Avatar, AvatarFallback } from './ui/avatar'
 
 interface Props {
-  id: string;
-  name: string;
-  imageUrl?: string | null;
-  className?: string;
+  id: string
+  name: string
+  imageUrl?: string | null
+  className?: string
+  fallbackClassName?: string
 }
 
-const UserAvatar = ({ id, name, imageUrl, className = 'h-9 w-9' }: Props) => {
+const UserAvatar = ({
+  id,
+  name,
+  imageUrl,
+  className = 'h-9 w-9',
+  fallbackClassName,
+}: Props) => {
   const initials = name
-    .split(" ")
+    .split(' ')
     .map((word: string) => word[0])
-    .join("")
+    .join('')
     .toUpperCase()
     .slice(0, 2)
+
   return (
     <Link href={ROUTES.PROFILE(id)}>
-      <Avatar className={className}>
+      <Avatar className={cn('relative', className)}>
         {imageUrl ? (
           <Image
             src={imageUrl}
+            alt={name}
             className="object-cover"
-            width={36}
-            height={36}
-            alt="avatar"
+            fill
+            quality={100}
           />
         ) : (
-          <AvatarFallback className="primary-gradient font-space-grotesk font-bold tracking-wider text-white">
+          <AvatarFallback
+            className={cn(
+              'primary-gradient font-space-grotesk font-bold tracking-wider text-white',
+              fallbackClassName
+            )}
+          >
             {initials}
           </AvatarFallback>
         )}
