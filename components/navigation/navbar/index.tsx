c3a256d867c4d69c151bffb03fa0dfcbdd5bad69
Amit Gupta <aryaamit387@gmail.com>
2026-07-21 21:48:20 +0530

    Name Fixed

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -23,7 +23,7 @@ const Navbar = async () => {
         />
 
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev<span className="text-primary-500">Overflow</span>
+          Dev<span className="text-primary-500">flow</span>
         </p>
       </Link>
 
